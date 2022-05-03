3d86fdd3e05f874b3254342b7d2e1439733d8472
Corey Held <corey.held@shp.org>
2022-05-03 14:41:05 -0400

    Initial Commit

---
+++ Weather App final2.0/Weather_App_final2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_App_final2_0App.swift
+//  Weather App final2.0
+//
+//  Created by Corey Held on 5/3/22.
+//
+
+import SwiftUI
+
+@main
+struct Weather_App_final2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
